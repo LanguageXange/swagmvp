1435d02fcb27d7c1f71df6a2642c6d7e609b1306
LanguageXange <hlin0017@seattlecentral.edu>
2021-11-20 21:12:42 -0800

    update lottie data directory

--- pages/store/[id].js
+++ pages/store/[id].js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { findAllSwags } from "../../util/airtable";
 import { handleUpvote } from "../../util/increaseVote";
 import Lottie from "react-lottie";
-import animationData from "../../public/asset/lottieheart.json";
+import animationData from "../../../asset/lottieheart.json";
 
 // increase vote - mario theme coin sound effect
 // home page use island illustration
